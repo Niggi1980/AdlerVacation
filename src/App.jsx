07883589ee3d4c57f336b3f9ee97e694c1a98531
Nick1980 <nickschulz2001@gmail.com>
2023-10-31 21:46:30 +0100

    Der Adler

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>Ein Adler der versucht hat zu fliegen, hast du versucht einzusperrt</div>
+  )
+}
+
+export default App
